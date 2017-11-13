c00c2262d25b57f05c6c353753a2873d9ba45351
oct2pus <30452393+oct2pus@users.noreply.github.com>
2017-11-13 14:56:01 -0600

    Delete member_join.rb

--- src/modules/responses/member_join.rb
+++
@@ -1,8 +0,0 @@
-module Bot::Responses
-    module Member_Join
-        extend Discordrb::EventContainer
-        member_join() do |event|
-            Bot::JADE.send_message(event.server.default_channel(),"hey **#{event.user.username}**, welcome to **#{event.server.name}**! :D")
-        end
-    end
-end
