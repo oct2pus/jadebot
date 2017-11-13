35d5d495df8db1bea0c5a33a4916f8bb698db173
oct2pus <30452393+oct2pus@users.noreply.github.com>
2017-11-13 14:56:09 -0600

    Delete member_leave.rb

--- src/modules/responses/member_leave.rb
+++
@@ -1,8 +0,0 @@
-module Bot::Responses
-    module Member_Leave
-        extend Discordrb::EventContainer
-        member_leave() do |event|
-            Bot::JADE.send_message(event.server.default_channel(),"**#{event.user.username}** left **#{event.server.name}**! D:")
-        end
-    end
-end
