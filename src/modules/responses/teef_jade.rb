2ced02b99fc397da197ee376a3d82b016075b724
Octopus Octopus <hiwayland@yahoo.com>
2017-11-13 19:28:22 -0600

    <:jadeteefs:317080214364618753>

--- src/modules/responses/teef_jade.rb
+++ src/modules/responses/teef_jade.rb
@@ -2,7 +2,7 @@ module Bot::Responses
     module Kiss_Jade
         extend Discordrb::EventContainer
         message(contains: /<:blobteefs/) do |event|
-            event.send_message(":jadeteefs:")
+            event.send_message("<:jadeteefs:317080214364618753>")
         end
     end
 end
