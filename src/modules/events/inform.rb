ca2c8922ff170d7bba7648d0b928ef3fb5ce4de0
Octopus Octopus <hiwayland@yahoo.com>
2017-11-15 18:14:26 -0600

    temporary file inform.rb

--- src/modules/events/inform.rb
+++ src/modules/events/inform.rb
@@ -3,7 +3,9 @@ module Bot::Events
     module Inform
         extend Discordrb::EventContainer
         ready() do |event|
-            Bot::JADE.send_message(Bot::JADE.server.default_channel(), "hey @everyone i am now storing messages users send, please read <https://github.com/oct2pus/jadebot/blob/master/STORAGE_POLICY.md> for more information!")
+            Bot::JADE.servers.each do |server|
+               Bot::JADE.send_message(server[1].default_channel(),  "hey @everyone i am now storing messages users send, please read <https://github.com/oct2pus/jadebot/blob/master/STORAGE_POLICY.md> for more information!")
+            end
         end
     end
 end
