f70cee23f6351677a0942ca44f5422155f41b05a
Octopus Octopus <hiwayland@yahoo.com>
2017-11-15 19:44:15 -0600

    temporary early release of embed messages for edits to fix issue with modified.rb's output

--- src/modules/events/modified.rb
+++ src/modules/events/modified.rb
@@ -17,9 +17,26 @@ module Bot::Events
                 redis = Redis.new
                 if redis.get(event.message.id) != nil
                     original_message = JSON.parse(redis.get(event.message.id))
-                    Bot::JADE.send_message(mod_log,"**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}** at #{event.timestamp}\noriginal message```#{original_message['message']}\n```new message\n```#{event.message}```")
+#                    Bot::JADE.send_message(mod_log,"**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}** at #{event.timestamp} original message\"#{original_message['message']}\"new message\"#{event.message}\"")
+                    mod_log.send_embed do |embed|
+                          embed.title = "Message Edited"
+                            embed.description = "**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}**"
+                            embed.timestamp = Time.now
+                            embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{Bot::JADE.profile.username}", icon_url: "#{Bot::JADE.profile.avatar_url}")
+                            
+                            embed.add_field(name: "Original", value: "```\n#{original_message['message']}```")
+                            embed.add_field(name: "New", value: "```\n#{event.message}```")
+                    end
                 else #fallback message if message was not stored
-                    Bot::JADE.send_message(mod_log,"**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}** at #{event.timestamp}")
+#                    Bot::JADE.send_message(mod_log,"**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}** at #{event.timestamp}")
+                    mod_log.send_embed do |embed|
+                            embed.description = "**#{event.user.username}##{event.user.tag}** has edited a message in **##{event.channel.name}**"
+                            embed.timestamp = Time.now
+                            embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{Bot::JADE.profile.username}", icon_url: "Bot::JADE.profile.avatar_url")
+                            
+                            embed.add_field(name: "Original", value: "Original Not Stored")
+                            embed.add_field(name: "New", value: "```\n#{event.message}```")
+                    end
                 end
             end
         end
