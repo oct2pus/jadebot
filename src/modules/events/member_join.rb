5a8631825b704fd70ee413649f6507eb7d8dda47
Octopus Octopus <hiwayland@yahoo.com>
2017-11-15 18:23:44 -0600

    notify new users that I store data

--- src/modules/events/member_join.rb
+++ src/modules/events/member_join.rb
@@ -5,7 +5,7 @@ module Bot::Events
         extend Discordrb::EventContainer
         member_join() do |event|
             mod_log =  event.server.text_channels.find { |c| c.name == 'mod-log' }
-            message = Bot::JADE.send_message(event.server.default_channel(),"hey **#{event.user.username}**, welcome to **#{event.server.name}**! :D")
+            message = Bot::JADE.send_message(event.server.default_channel(),"hey **#{event.user.username}**, welcome to **#{event.server.name}**! :D\n please read <https://github.com/oct2pus/jadebot/blob/master/STORAGE_POLICY.md> for information on what data I store on you!")
             if mod_log == nil
                 if Bot::JADE.profile.on(event.server).permission?(:manage_server)
                     mod_log = event.server.create_channel("mod-log")
