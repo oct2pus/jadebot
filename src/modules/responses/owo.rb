4efd7412806916251f75b2ba7bd2cf97b528fd77
Octopus Octopus <hiwayland@yahoo.com>
2017-11-26 19:25:02 -0600

    Fix provided by TJB0607 to correct words that contain 'owo' (like c'owo'rker) from triggering 'what's this :o'

--- src/modules/responses/owo.rb
+++ src/modules/responses/owo.rb
@@ -3,7 +3,7 @@
 module Bot::Responses
     module OwO
         extend Discordrb::EventContainer
-        message(contains: /owo/i) do |event|
+        message(contains: /\bowo\b/i) do |event|
             event.send_message("whats this :o")
         end
     end
