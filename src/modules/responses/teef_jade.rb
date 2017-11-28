1dae0f0da035444e138fd24a535fb07ceb740cb8
Octopus Octopus <hiwayland@yahoo.com>
2017-11-27 19:30:44 -0600

    fixed typo in code

--- src/modules/responses/teef_jade.rb
+++ src/modules/responses/teef_jade.rb
@@ -4,7 +4,7 @@ module Bot::Responses
     module Kiss_Jade
         extend Discordrb::EventContainer
         message(contains: [/<:blobteefs/, /<:jadeteefs/]) do |event|
-			if Bot::JADE.profile.on(event.server).has_permission?(:use_external_emoji)
+			if Bot::JADE.profile.on(event.server).permission?(:use_external_emoji)
           		event.send_message("<:jadeteefs:317080214364618753>")
 			end
         end
