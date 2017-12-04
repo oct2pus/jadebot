552f9f226380dc8b9a174d31ec96e8915bc3ca7c
Octopus Octopus <hiwayland@yahoo.com>
2017-12-03 23:19:18 -0600

    User_post.rb should now respect voice activity

--- src/modules/events/user_post.rb
+++ src/modules/events/user_post.rb
@@ -10,15 +10,19 @@ module Bot::Events
 	extend Discordrb::EventContainer
 		message() do |event|
 				redis = Redis.new
+
 			if Bot::JADE.profile.on(event.server).permission?(:manage_server) && Bot::JADE.profile.on(event.server).permission?(:manage_channels)
 				message_hash = {:user => event.message.user.username, :tag => event.message.user.tag, :message => event.message.content, :avatar => event.message.user.avatar_url}
 				redis.set event.message.id, message_hash.to_json
 				redis.expire(event.message.id, 18000) #in seconds, equal to five hours
 			end
-			if Bot::JADE.profile.on(event.server).permission?(:read_messages) && Bot::JADE.profile.on(event.server).permission?(:use_voice_activity)
+
+			if Bot::JADE.profile.on(event.server).permission?(:use_voice_activity)
 				levels = Bot::DB[:levels].where(user_id: event.message.user.id, server_id: event.server.id)
+
 				if levels.empty?
 					levels.insert(user_id: event.message.user.id, server_id: event.server.id, level: 1, xp: 0, to_next_level: 100)
+
 				elsif redis.get("#{event.message.user.id}:#{event.server.id}:level_lock") == nil
 					user_level = levels.get(:level)
 					user_xp = levels.get(:xp)
