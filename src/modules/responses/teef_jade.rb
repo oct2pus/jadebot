820c4e98bdb9211b55091d46ccde3ac63d3c2d40
Octopus Octopus <hiwayland@yahoo.com>
2017-12-06 20:33:02 -0600

    fixed mistake inserted in last commit

--- src/modules/responses/teef_jade.rb
+++ src/modules/responses/teef_jade.rb
@@ -7,7 +7,7 @@ module Bot
       message(contains: [/<:blobteefs/, /<:jadeteefs/]) do |event|
         if Bot::JADE.profile.on(event.server).permission?(:use_external_emoji)
           event.send_message('<:jadeteefs:317080214364618753>')
-        end``
+        end
       end
     end
   end
