b1b183c2db194bc872c330769672138fc79da981
Octopus Octopus <oct2pus@jade.moe>
2018-01-21 08:30:50 -0600

    temporary addition to start_up to warn admins of removal of mod-log features.

--- src/modules/events/start_up.rb
+++ src/modules/events/start_up.rb
@@ -15,9 +15,14 @@ module Bot
           counter += 1
           puts "#{counter}: #{server.name} started loading users."
           if Bot::JADE.profile.on(server).permission?(:manage_server) && Bot::JADE.profile.on(server).permission?(:manage_channels)
+              mod_log = server.text_channels.find { |c| c.name == 'mod-log' }
+              mod_log = server.create_channel('mod-log') if mod_log.nil?
+
+              mod_log.send_message("@everyone heyo, im removing jadebot's moderation features in 48 hours because of a desire to focus on what make jadebot unique(being cute and commands useful for homestuck servers), I have a new moderation bot here you can use or you can swap to someone else's solution, I won't be mad, I promise. Please invite https://discordapp.com/oauth2/authorize?client_id=404624723595493378&scope=bot&permissions=268790838 to continue using 'jadebot's mod-log.")
             server.members.each do |member|
               redis.set "#{server.id}:#{member.id}", member.display_name.to_s
             end
+            sleep(20)
           else
             puts "#{counter}: #{server.name} does not have :manage_server and/or :manage_channels permissions, skipping."
           end
