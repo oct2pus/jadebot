3ba596f49b8a65fde91c067d353df3090e7369be
Octopus Octopus <oct2pus@jade.moe>
2018-01-21 06:38:53 -0600

    updated >wiki to account for fail conditions (e.g. bad searches or what have you)

--- src/modules/commands/wiki.rb
+++ src/modules/commands/wiki.rb
@@ -1,5 +1,4 @@
 require 'rest-client'
-require 'nori'
 require 'redis'
 require 'json'
 
@@ -7,44 +6,47 @@ module Bot
   module Commands
     module Wiki
       extend Discordrb::Commands::CommandContainer
-      command :wiki do |event, *args|
-
-        words = args.join(' ')
-
-        # variable objects
+      command(:wiki, description: "searches the mspa wiki\nusage: >wiki `what you want to search`") do |event, *args|
         redis = Redis.new
+        if !redis.exists("#{event.server.id}:wikilock")
 
-        # variables relating to search_url
-        limit = 1
-        batch = 1
-        min_article_quality = 10
-        search = args.join('+')
+          limit = 1
+          batch = 1
+          min_article_quality = 10
+          search = args.join('+')
+          words = args.join(' ')
 
-        # url to be parsed
-        search_url = "http://mspaintadventures.wikia.com/api/v1/Search/List?query=#{search}&limit=#{limit}&minArticleQuality=#{min_article_quality}&batch=#{batch}"
+          # url to be parsed
+          search_url = "http://mspaintadventures.wikia.com/api/v1/Search/List?query=#{search}&limit=#{limit}&minArticleQuality=#{min_article_quality}&batch=#{batch}"
 
-        event.channel.send_temporary_message("searching for #{words}", 5)
+          event.channel.send_temporary_message("searching for #{words}", 5)
 
-        # items is an array but will only hold one value if you set limit to one
-        begin
-          search = JSON.parse(RestClient.get(search_url))
-        rescue StandardError
-          event.send_message("I couldn't find anything about \"#{words}\" :(")
-        else
-          id = search["items"][0]["id"]
-          actual_article_url = search["items"][0]["url"]
+          # items is an array but will only hold one value if you set limit to one
+          begin
+            search = JSON.parse(RestClient.get(search_url))
+          rescue StandardError
+            event.send_message("I couldn't find anything about \"#{words}\" :(")
+          else
+            redis.set "#{event.server.id}:wikilock", true
+            redis.expire("#{event.server.id}:wikilock", 7)
 
-          json_article_url = "http://mspaintadventures.wikia.com/api/v1/Articles/AsSimpleJson?id=#{id}"
-          article = JSON.parse(RestClient.get(json_article_url))
+            id = search["items"][0]["id"]
+            actual_article_url = search["items"][0]["url"]
 
-          title = article["sections"][0]["title"]
-          text = article["sections"][0]["content"][0]["text"]
+            json_article_url = "http://mspaintadventures.wikia.com/api/v1/Articles/AsSimpleJson?id=#{id}"
+            article = JSON.parse(RestClient.get(json_article_url))
 
-          event.channel.send_embed("heres what i found about \"#{words}\" :U") do |embed|
-            embed.title = title
-            embed.url = actual_article_url
-            embed.description = text
+            title = article["sections"][0]["title"]
+            text = article["sections"][0]["content"][0]["text"]
+
+            event.channel.send_embed("heres what i found about \"#{words}\" :U") do |embed|
+              embed.title = title
+              embed.url = actual_article_url
+              embed.description = text
+            end
           end
+        else
+            event.send_temporary_message("please slow down!\nwait another #{redis.ttl("#{event.server.id}:wikilock")} seconds :p", redis.ttl("#{event.server.id}:wikilock"))
         end
       end
     end
