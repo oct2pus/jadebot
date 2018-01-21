38ec3eef008ae07ebde7ee03b346d1f218f8ad2f
Octopus Octopus <oct2pus@jade.moe>
2018-01-21 06:37:00 -0600

    removed leveling

--- src/modules/events/user_post.rb
+++ src/modules/events/user_post.rb
@@ -10,7 +10,6 @@ module Bot
       extend Discordrb::EventContainer
       message do |event|
         redis = Redis.new
-
         if Bot::JADE.profile.on(event.server).permission?(:manage_server) && Bot::JADE.profile.on(event.server).permission?(:manage_channels)
           message_hash = { user: event.message.user.username, tag: event.message.user.tag, message: event.message.content, avatar: event.message.user.avatar_url }
           redis.set event.message.id, message_hash.to_json
@@ -18,30 +17,7 @@ module Bot
           # little more than 41 hours
         end
 
-        if Bot::JADE.profile.on(event.server).permission?(:use_voice_activity)
-          levels = Bot::DB[:levels].where(user_id: event.message.user.id, server_id: event.server.id)
-
-          if levels.empty?
-            levels.insert(user_id: event.message.user.id, server_id: event.server.id, level: 1, xp: 0, to_next_level: 100)
-
-          elsif redis.get("#{event.message.user.id}:#{event.server.id}:level_lock").nil?
-            user_level = levels.get(:level)
-            user_xp = levels.get(:xp)
-            user_next = levels.get(:to_next_level)
-
-            user_xp += rand(10..31)
-
-            if user_xp >= user_next
-              user_next += user_next
-              user_level += 1
-              event.send_message("Level up!\n#{event.message.user.display_name} is now **Level #{user_level}**")
-            end
-            levels.update(level: user_level, xp: user_xp, to_next_level: user_next)
-            redis.set "#{event.message.user.id}:#{event.server.id}:level_lock", true
-            redis.expire("#{event.message.user.id}:#{event.server.id}:level_lock", 30)
-          end
-          redis.close
-        end
+        redis.close
       end
     end
   end
