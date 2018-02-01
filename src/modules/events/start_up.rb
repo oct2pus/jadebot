84d98a5a05e315ddd4d97b0f6701555c7a4245b4
Octopus Octopus <oct2pus@jade.moe>
2018-01-31 20:54:14 -0600

    sign

--- src/modules/events/start_up.rb
+++ src/modules/events/start_up.rb
@@ -13,11 +13,7 @@ module Bot
           puts "adding settings to #{check.name}"
           redis.set "#{check.id}:GREETER", true
           redis.set "#{check.id}:GREETER_CHANNEL", check.default_channel.id
-          if Bot::JADE.profile.on(check).permission?(:send_messages, check.default_channel)
-            check.default_channel.send_message("i now have a >setting command, you can use it to disable the new user greeter/leaver and select what channel i send the message to, because of how discord has changed how default channels are handled I will instead send messages to the first channel i have send message permissions on, I recommend changing this to your prefered channel with ``>settings greeter_channel`. i apologize for the intrusion and i hope you have a lovely day!")
-          end
           puts "sent message to #{check.name}, sleeping for 60 seconds"
-          sleep(60)
         end
       end
     end
