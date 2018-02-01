30a3af54c5f0b02057d887f68332d8d98e9794c9
Octopus Octopus <oct2pus@jade.moe>
2018-01-31 20:39:22 -0600

    disable temporary start up event.

--- src/modules/events/start_up.rb
+++ src/modules/events/start_up.rb
@@ -1,5 +1,3 @@
-require 'redis'
-
 module Bot
   module Events
     # commands to run when bot starts running
@@ -9,15 +7,8 @@ module Bot
         redis = Redis.new
         puts "Total Number of Servers: #{Bot::JADE.servers.size}"
         puts 'Jadebot initalized!'
-        Bot::JADE.servers.values.each do |check|
-          puts "adding settings to #{check.name}"
-          redis.set "#{check.id}:GREETER", true
-          redis.set "#{check.id}:GREETER_CHANNEL", check.default_channel.id
-          check.default_channel.send_message("i now have a >setting command, you can use it to disable the new user greeter/leaver and select what channel i send the message to, because of how discord has changed how default channels are handled I will instead send messages to the first channel i have send message permissions on, I recommend changing this to your prefered channel with ``>settings greeter_channel`. i apologize for the intrusion and i hope you have a lovely day!")
-          puts "sent message to #{check.name}, sleeping for 60 seconds"
-          sleep(60)
-        end
       end
     end
   end
 end
+``
