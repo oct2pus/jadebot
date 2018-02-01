2f8e732f22e1bd6b659a2775f1a818f60d3855ac
Octopus Octopus <oct2pus@jade.moe>
2018-01-31 20:59:14 -0600

    added a server create event so settings are added to new servers

---
+++ src/modules/events/server_create.rb
@@ -0,0 +1,15 @@
+require 'redis'
+
+module Bot
+  module Events
+    # commands to run when bot starts running
+    module ServerCreate
+      extend Discordrb::EventContainer
+      server_create do |_event|
+        redis = Redis.new
+        redis.set "{event.server.id}:GREETER", true
+        redis.set "{event.server.id}:GREETER_CHANNEL", event.server.default_channel.id
+      end
+    end
+  end
+end
