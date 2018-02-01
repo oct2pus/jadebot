95af2d0bda01ef0fee1a067533a91141874c3203
Octopus Octopus <oct2pus@jade.moe>
2018-01-31 20:15:44 -0600

    added ability to control member leave/join messages

--- src/modules/events/member_leave.rb
+++ src/modules/events/member_leave.rb
@@ -1,13 +1,17 @@
+require 'redis'
+
 module Bot
   module Events
     # event logs every time a user leaves as well as publically annouces it
     module MemberLeave
       extend Discordrb::EventContainer
       member_leave do |event|
-        if Bot::JADE.profile.on(event.server).permission?(:use_voice_activity)
-          default_channel = event.server.default_channel
+        redis = Redis.new
+        if redis.exists("#{event.server.id}:GREETER") && redis.get("#{event.server.id}:GREETER")
+          channel_id = redis.get("#{event.server.id}:GREETER_CHANNEL").to_s
+          default_channel = event.server.text_channels.find { |c| c.id.to_s == channel_id }
           if default_channel.nil?
-            default_channel = event.server.text_channels.find { |c| c.name == 'general' }
+            default_channel = event.server.default_channel
           end
           # event.server.default_channel.send_embed("goodbye! D:") do |embed|
           default_channel.send_embed('goodbye! D:') do |embed|
@@ -18,6 +22,7 @@ module Bot
             embed.add_field(name: 'a user has left! D:', value: "so long,\n**#{event.user.username}##{event.user.tag}**!")
           end
         end
+        redis.close
       end
     end
   end
