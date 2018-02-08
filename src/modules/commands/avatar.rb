dc19d18757f520befb2b585f8dd1c630a5759957
Octopus Octopus <oct2pus@jade.moe>
2018-02-08 02:29:10 -0600

    updated >avatar to use .png instead of .webp

--- src/modules/commands/avatar.rb
+++ src/modules/commands/avatar.rb
@@ -9,12 +9,16 @@ module Bot
         end
 
         if user_chosen.nil?
-          event << 'please **mention** a valid user'
+          event.channel.send_embed("here is **your** avatar!") do |embed|
+            embed.title = 'View Source'
+            embed.url = event.user.avatar_url.gsub('.webp', '.png')
+            embed.image = { url: event.user.avatar_url.gsub('.webp', '.png') }
+          end
         else
           event.channel.send_embed("here is **#{user_chosen.on(event.server).display_name}**'s avatar!") do |embed|
             embed.title = 'View Source'
-            embed.url = user_chosen.avatar_url
-            embed.image = { url: user_chosen.avatar_url }
+            embed.url = user_chosen.avatar_url.gsub('.webp', '.png')
+            embed.image = { url: user_chosen.avatar_url.gsub('.webp', '.png') }
           end
         end
       end
