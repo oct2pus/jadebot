367b284994998894168dffb3e3ac073a00306b8a
Austin LaBerta <oct2pus@debian.pc>
2018-02-25 00:10:05 -0600

    santized input on >otp

--- src/modules/commands/otp.rb
+++ src/modules/commands/otp.rb
@@ -1,6 +1,6 @@
 module Bot
   module Commands
-    # command posts a link to the jadebot github page
+    # Command determines a pairings viability
     module Otp
       extend Discordrb::Commands::CommandContainer
       command(:otp, description: "ship some characters together!\nusage: `>otp as many characters as you want`") do |event, *args|
@@ -8,21 +8,27 @@ module Bot
           event.send_message('i dont know what you want me to ship :\\')
         else
 
+          # sanitize input
+          all_args = args.join(' ')
+          all_args = all_args.gsub('`', '\\\`')
+          all_args = all_args.gsub('@here', '`@here`')
+          all_args = all_args.gsub('@everyone', '`@everyone`')
+
           # math
-          all_args = args.join('')
           tally = 0
 
           all_args.split(//).each do |arg|
             tally += arg.ord
           end
 
-          tally = (tally*2 % 100) + 1
+          tally = (tally * 2 % 100) + 1
 
-          # display
           ush = (tally / 10).to_i # ultra simple hash - thanks tjb
           hearts = ':heart:' * ush
-          unhearts = ':broken_heart:' * (10 - ush) 
-          message = "id say that #{args.join(' ')} has about a #{tally}% chance of being canon! :D \n"
+          unhearts = ':broken_heart:' * (10 - ush)
+
+          # display
+          message = "id say that #{all_args} has about a #{tally}% chance of being canon! :D \n"
           message << hearts + unhearts
           event.send_message(message)
         end
