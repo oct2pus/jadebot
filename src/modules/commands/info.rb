c1c21ccbb4dcef17f13b8a0c75341acb7f7b8246
Austin LaBerta <oct2pus@debian.pc>
2018-04-06 21:33:22 -0500

    added >info

---
+++ src/modules/commands/info.rb
@@ -0,0 +1,20 @@
+module Bot
+  module Commands
+    # command posts a user avatar
+    module Info
+      extend Discordrb::Commands::CommandContainer
+      Bot::JADE.bucket :info_b, limit: 1, time_span: 20
+      command(:info, bucket: :info_b, description: 'displays information about jadebot!') do |event|
+        event.channel.send_embed do |embed|
+          embed.title = 'Jadebot Development Information'
+          embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: "#{Bot::JADE.profile.avatar_url.gsub('.webp', '.png')}")
+          embed.add_field(name: 'Developer:', value: "**Discord**: *🐙🐙#0413*\n**Tumblr**: https://oct2pus.tumblr.com\n**Mastodon**: https://im-in.space/@oct2pus\n**Github**: https://github.com/oct2pus/\n")
+          embed.add_field(name: 'Discord Library:', value: "Discordrb: https://github.com/meew0/discordrb", inline: true)
+          embed.add_field(name: 'Special Thanks:', value: "*tjb#0607* for his contributions\n*Andrew Hussie* for creating Homestuck\n*Jade Harley* for being herself")
+          embed.add_field(name: 'Disclaimer:"', value: "Jadebot uses **Mutant Standard Emoji** (https://mutant.tech) made by *Dzuk#1671*\n**Mutant Standard Emoji** are licensed under CC-BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/) ")
+          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "💚 Prefix: #{Pre::FIX} | Servers: #{Bot::JADE.servers.size}")
+        end
+      end
+    end
+  end
+end
