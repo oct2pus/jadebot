1c0f7abeea36aab24ccc352fa5da3439272ebf96
Octopus Octopus <oct2pus@jade.moe>
2018-04-07 22:41:47 -0500

    avatar uses new Jb.fuzz command to determine user

--- src/modules/commands/avatar.rb
+++ src/modules/commands/avatar.rb
@@ -5,8 +5,10 @@ module Bot
       extend Discordrb::Commands::CommandContainer
       Bot::JADE.bucket :avatar_b, limit: 3, time_span: 20, delay: 5
       command(:avatar, bucket: :avatar_b, rate_limit_message: 'slowdown fuckass! wait %time% more seconds before doing that again', description: "gets a users avatar\nusage: #{Pre::FIX}avatar `@user`\n please @ the user") do |event, user_chosen|
-        unless user_chosen.nil?
-          user_chosen = Bot::JADE.parse_mention(user_chosen)
+        if Bot::JADE.parse_mention(user_chosen)
+          user = Bot::JADE.parse_mention(user_chosen).on(event.server)
+      elsif !user_chosen.nil?
+          user = Jb.fuzz(event, user_chosen)
         end
 
         if user_chosen.nil?
@@ -16,13 +18,14 @@ module Bot
             embed.image = { url: event.user.avatar_url.gsub('.webp', '.png') }
           end
         else
-          event.channel.send_embed("here is **#{user_chosen.on(event.server).display_name}**'s avatar!") do |embed|
+          event.channel.send_embed("here is **#{user.display_name}**'s avatar!") do |embed|
             embed.title = 'View Source'
-            embed.url = user_chosen.avatar_url.gsub('.webp', '.png')
-            embed.image = { url: user_chosen.avatar_url.gsub('.webp', '.png') }
+            embed.url = user.avatar_url.gsub('.webp', '.png')
+            embed.image = { url: user.avatar_url.gsub('.webp', '.png') }
           end
         end
       end
+
     end
   end
 end
