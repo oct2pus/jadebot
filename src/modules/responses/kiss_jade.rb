7eadf539d80b8f1985679569b2454e831ef07e0e
Austin LaBerta <oct2pus@debian.pc>
2018-04-08 21:02:51 -0500

    new emoji output for :kissjade:

--- src/modules/responses/kiss_jade.rb
+++ src/modules/responses/kiss_jade.rb
@@ -7,7 +7,7 @@ module Bot
       message(contains: /<:kissjade/) do |event|
         server_settings = JSON.parse(Re::DIS.get("#{event.server.id}:SETTINGS"))
         if server_settings['interaction'] == 2
-          event.send_message(':flushed::two_hearts:')
+          event.send_message('<:jb_embarassed:432675788698157056><:jade_hearts:432685108085129246>')
         end
       end
     end
