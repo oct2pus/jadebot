f154e4dbd89e56fc62e8a30a1164817e538375ce
Octopus Octopus <oct2pus@jade.moe>
2018-06-12 18:14:52 -0500

    poll creation now works

--- src/modules/commands/poll.rb
+++ src/modules/commands/poll.rb
@@ -1,4 +1,6 @@
 #frozen_string_literal: false
+# note to future me: if shit is getting weird w/ strawpoll its because i didnt
+# bother to impliment rate limiting since its 100 per hour
 
 module Bot
   module Commands
@@ -18,7 +20,7 @@ module Bot
             end
           end
           
-          @@multi = multi
+          @@multi = multi # true if users can vote on multiple options
 
         end
         def to_h
@@ -32,11 +34,42 @@ module Bot
         end
       end
       extend Discordrb::Commands::CommandContainer
-      command(:poll, description: "starts a strawpoll") do |event, *args|
-        sArgs = args.join(" ") #args as String
-        pArgs = sArgs.split("|") #args processed
-        event.send_message(Poll_builder.new(pArgs, false).to_h)
-        nil
+      command(:poll, description: "starts a strawpoll\nusage: #{Pre::FIX}poll title | option 1 | option 2 | ... | option n\n returns the current poll if there is an ongoing poll\nall polls expire after 24 hours") do |event, *args|
+
+        unless !event.user.permission? :administrator
+          sArgs = args.join(" ") # args as a string
+          pArgs = sArgs.split("|") # args split into new array
+          if Re::DIS.exists "#{event.server.id}:#{event.channel.id}:POLL"
+            event.send_message("The Current Poll is: #{Re::DIS.get "#{event.server.id}:#{event.channel.id}:POLL"}")
+          else
+            break if sArgs.empty? || pArgs.length <= 2
+            poll = Poll_builder.new(pArgs, false).to_h.to_json
+
+            begin
+              request = RestClient.post(
+                'https://strawpoll.me/api/v2/polls',
+                poll,
+                content_type: "application/json"
+              )
+              rescue RestClient::ExceptionWithResponse => err # follow 302 response
+              result = JSON.parse(err.response.follow_redirection)
+            end
+
+            url = "https://strawpoll.me/#{result['id']}"
+            event.send_message("here is your poll :D\n#{url}")
+            Re::DIS.set "#{event.server.id}:#{event.channel.id}:POLL", url
+            Re::DIS.expire "#{event.server.id}:#{event.channel.id}:POLL", 86400 # 24 hours
+            nil
+          end
+          # todo: write fail conditions
+          # if sArgs.empty? && !Re::DIS.exists("#{event.server.id}:#{event.channel.id}:POLL")
+          #   event.send_temporary_message("please tell me a title and two or more options\nsee #{Pre::FIX}help poll for info", 5)
+          # elsif pArgs <= 2 && !Re::DIS.exists("#{event.server.id}:#{event.channel.id}:POLL")
+          #   event.send_temporary_message("i need at least two or more options\nsee #{Pre::FIX}help poll for info", 5)
+          # end
+        else
+          event.send_temporary_message("only administrators can create polls :(", 5)
+        end
       end
     end
   end
