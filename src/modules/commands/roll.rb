7a8760251a2ad1947b4f94833d376942262c0acf
Octopus Octopus <oct2pus@jade.moe>
2018-07-21 16:50:37 -0500

    reorganized code, added >lroll and >hroll

--- src/modules/commands/roll.rb
+++ src/modules/commands/roll.rb
@@ -6,81 +6,6 @@ module Bot
     module Roll
       extend Discordrb::Commands::CommandContainer
 
-      # return all rolls; input is an array
-      def self.get_rolls(input)
-        rolls = Array.new(input[0])
-
-        rolls.each_index do |roll|
-          rolls[roll] = rand(input[1]) + 1
-        end
-
-         rolls
-      end
-
-      # writes the embed
-      def self.write_embed(dice_message, input, mod_sign, rolls, total, die_image, event)
-
-        output = '`'
-        rolls.each_index do |roll|
-          output += "`\n`" if roll % 4 == 0 && roll != 0
-          output += "\t" if roll == 0 || roll % 4 != 0
-          output += "|#{rolls[roll].to_s.center(3)}|"
-        end
-          output += '`'
-
-        # catches edgecase that would fuck over formatting
-        if output.end_with?('``')
-          output.slice!((output.length - 1)...(output.length))
-        end
-
-        mod_out = '+'
-        unless mod_sign
-          mod_out = '-'
-        end
-
-        # output
-        event.channel.send_embed do |embed|
-          embed.footer = Discordrb::Webhooks::EmbedFooter.new(icon_url: die_image, text: dice_message.to_s)
-          embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: die_image)
-          embed.add_field(name: 'Rolls', value: output.to_s, inline: true)
-          embed.add_field(name: 'Modifier', value: "#{mod_out}#{input[2]}", inline: true)
-          embed.add_field(name: 'Results', value: total.to_s, inline: true)
-        end
-      end
-
-
-      # gets die image used in write_embed
-      def self.get_die_image(die_size)
-        die = {
-          d2: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/coin.png',
-          d4: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d4.png',
-          d6: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d6.png',
-          d8: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d8.png',
-          d10: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d10.png',
-          d12: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d12.png',
-          d20: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d20.png'
-        }
-        image_url = ''
-        image_url = case die_size
-        when 13..120
-          die[:d20]
-        when 11..12
-          die[:d12]
-        when 9..10
-          die[:d10]
-        when 7..8
-          die[:d8]
-        when 5..6
-          die[:d6]
-        when 3..4
-          die[:d4]
-        else
-          die[:d2]
-        end
-
-        return image_url
-      end
-      
       # validates input
       def self.valid_message(message, event)
         validity = true
@@ -116,7 +41,6 @@ module Bot
 
         input = input.keep_if { |a| a =~ /[0-9]/ }
 
-        
         # to_i conversions to make sure no funny business happens
         input[0] = input[0].to_i
         input[1] = input[1].to_i
@@ -151,9 +75,51 @@ module Bot
         mod_sign
       end
 
+      # return all rolls; input is an array
+      def self.get_rolls(input)
+        rolls = Array.new(input[0])
+
+        rolls.each_index do |roll|
+          rolls[roll] = rand(input[1]) + 1
+        end
+
+         rolls
+      end
+
+      # gets the total of all die rolls added together
       def self.get_total(rolls, modifier, mod_sign)
         total = 0
         rolls.each { |roll| total = total + roll}
+
+        total = add_mod(total, modifier, mod_sign)
+
+        total
+      end
+
+      # finds the highest or lowest die roll, adds modifier and uses that as total
+      # true for highest, false for lowest
+      def self.get_total_skewed(rolls, modifier, mod_sign, type)
+        total = rolls[0]
+
+        rolls.each do |roll|
+          if type
+            if roll > total
+              total = roll
+            end
+          else
+            if roll < total
+              total = roll
+            end
+          end
+        end
+
+        total = add_mod(total, modifier, mod_sign)
+
+        total
+      end
+
+      # adds the modifier to total
+      def self.add_mod(total, modifier, mod_sign)
         if mod_sign
           total = total + modifier
         else
@@ -162,34 +128,130 @@ module Bot
 
         total
       end
+      
+      # gets die image used in write_embed
+      def self.get_die_image(die_size)
+        die = {
+          d2: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/coin.png',
+          d4: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d4.png',
+          d6: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d6.png',
+          d8: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d8_vriska.png',
+          d10: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d10_alt.png',
+          d12: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d12.png',
+          d20: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/d20.png'
+        }
+        image_url = ''
+        image_url = case die_size
+        when 13..120
+          die[:d20]
+        when 11..12
+          die[:d12]
+        when 9..10
+          die[:d10]
+        when 7..8
+          die[:d8]
+        when 5..6
+          die[:d6]
+        when 3..4
+          die[:d4]
+        else
+          die[:d2]
+        end
+
+        return image_url
+      end
+
+      # writes the embed
+      def self.write_embed(dice_message, input, mod_sign, rolls, total, die_image, event)
+
+        output = '`'
+        rolls.each_index do |roll|
+          output += "`\n`" if roll % 4 == 0 && roll != 0
+          output += "\t" if roll == 0 || roll % 4 != 0
+          output += "|#{rolls[roll].to_s.center(3)}|"
+        end
+          output += '`'
+
+        # catches edgecase that would fuck over formatting
+        if output.end_with?('``')
+          output.slice!((output.length - 1)...(output.length))
+        end
+
+        mod_out = '+'
+        unless mod_sign
+          mod_out = '-'
+        end
+
+        # output
+        event.channel.send_embed do |embed|
+          embed.footer = Discordrb::Webhooks::EmbedFooter.new(icon_url: die_image, text: dice_message.to_s)
+          embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: die_image)
+          embed.add_field(name: 'Rolls', value: output.to_s, inline: true)
+          embed.add_field(name: 'Modifier', value: "#{mod_out}#{input[2]}", inline: true)
+          embed.add_field(name: 'Results', value: total.to_s, inline: true)
+        end
+      end
 
       # Commands
 
       command(:roll, description: "roll up to 20 dice\nusage: #{Pre::FIX}roll `NdN+-N`") do |event, dice_message|
         # process
         if valid_message(dice_message, event)
-          puts "dice_message: #{dice_message}"
 
           input = split_input(dice_message)
-          input.each { |x| puts "input: #{x}" }
           
           mod_sign = get_mod_sign(dice_message)
-          puts "mod_sign: #{mod_sign}"
           
           rolls = get_rolls(input)
-          puts "rolls: #{rolls}"
           
           total = get_total(rolls, input[2], mod_sign)
-          puts total
 
           die_image = get_die_image(input[1])
-          puts "die_image: #{die_image}"
-          nil
 
           write_embed(dice_message, input, mod_sign, rolls, total, die_image, event)
 
         end
       end
+
+      command(:hroll, description: "roll up to 20 dice\nonly the highest value is added to the total\nusage: #{Pre::FIX}hroll `NdN+-N`") do |event, dice_message|
+        # process
+        if valid_message(dice_message, event)
+
+          input = split_input(dice_message)
+          input.each { |x| puts "input: #{x}" }
+          
+          mod_sign = get_mod_sign(dice_message)
+          
+          rolls = get_rolls(input)
+          
+          total = get_total_skewed(rolls, input[2], mod_sign, true)
+
+          die_image = get_die_image(input[1])
+          nil
+
+          write_embed(dice_message, input, mod_sign, rolls, total, die_image, event)
+        end
+      end
+
+      command(:lroll, description: "roll up to 20 dice\nonly the lowest value is added to the total\nusage: #{Pre::FIX}lroll `NdN+-N`") do |event, dice_message|
+        # process
+        if valid_message(dice_message, event)
+
+          input = split_input(dice_message)
+          
+          mod_sign = get_mod_sign(dice_message)
+          
+          rolls = get_rolls(input)
+          
+          total = get_total_skewed(rolls, input[2], mod_sign, false)
+
+          die_image = get_die_image(input[1])
+          nil
+
+          write_embed(dice_message, input, mod_sign, rolls, total, die_image, event)
+        end
+      end
+      
     end
   end
 end
