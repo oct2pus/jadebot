7fd20108b3f8b6c48f88984a99b07dc6e345ffd3
Octopus Octopus <oct2pus@jade.moe>
2018-08-23 14:26:10 -0500

    updated info.rb

--- src/modules/commands/info.rb
+++ src/modules/commands/info.rb
@@ -12,7 +12,7 @@ module Bot
           embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: Bot::JADE.profile.avatar_url.gsub('.webp', '.png').to_s)
           embed.add_field(name: 'Developer:', value: "**Discord**: *🐙🐙#0413*\n**Tumblr**: https://oct2pus.tumblr.com\n**Mastodon**: https://im-in.space/@oct2pus\n**Github**: https://github.com/oct2pus/\n")
           embed.add_field(name: 'Discord Library:', value: 'Discordrb: https://github.com/meew0/discordrb', inline: true)
-          embed.add_field(name: 'Special Thanks:', value: "*tjb#0607* (http://blog.tjb0607.me) for his contributions\n*taiyoooh* (https://taiyoooh.tumblr.com/) for Jadebot's avatar\n*Andrew Hussie* (https://homestuck.com) for creating Homestuck\n*Jade Harley* for being herself!")
+          embed.add_field(name: 'Special Thanks:', value: "*tjb#0607* (http://blog.tjb0607.me) for his contributions\n*taiyoooh* (https://taiyoooh.tumblr.com/) for Jadebot's original avatar\n*chuchumi* (https://chuchumi.tumblr.com) for Jadebot's current avatar\n*Andrew Hussie* (https://homestuck.com) for creating Homestuck\n*Jade Harley* for being herself!")
           embed.add_field(name: 'Disclaimer:"', value: "Jadebot uses **Mutant Standard Emoji** (https://mutant.tech) made by *Dzuk#1671*\n**Mutant Standard Emoji** are licensed under CC-BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/) ")
           embed.footer = Discordrb::Webhooks::EmbedFooter.new(icon_url: 'https://raw.githubusercontent.com/oct2pus/jadebot/master/src/art/jade_heart.png', text: " Prefix: #{Pre::FIX} | Servers: #{Bot::JADE.servers.size}")
         end
