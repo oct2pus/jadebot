a88867504bda9a5d01f29e810849fb40034e88ce
octopus octopus <oct2pus@jade.moe>
2019-10-16 18:22:53 -0500

    fixed syntax errors

--- command/command.go
+++ command/command.go
@@ -58,7 +58,7 @@ func Booru(bot bocto.Bot,
 	if len(input) > 0 {
 		uri += "&tags="
 		for _, ele := range input {
-			uri += url.QueryEscape(ele) + "+"(
+			uri += url.QueryEscape(ele) + "+"
 		}
 		uri = strings.TrimSuffix(uri, "+")
 	}
@@ -67,7 +67,7 @@ func Booru(bot bocto.Bot,
 	// tags include art that would break the ToS;
 	// remove stuff at your own peril.
 	uri += "+-gore+-vomit+-bondage+-dubcon+-mind_control+-swimsuit+-blood" +
-		"-undergarments+-biting+-rating:questionable+-rating:explicit" +
+		"+-undergarments+-biting+-rating:questionable+-rating:explicit" + 
 		"+-nsfwsource+-deleteme"
 
 	// hardcoded 'do not want' tags, these are personal judgments by me.
