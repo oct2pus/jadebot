f1d704f3a10e39d7a27a213692e410e4dff456cc
octopus octopus <oct2pus@jade.moe>
2019-11-10 23:12:30 -0600

    add db.go

---
+++ db/db.go
@@ -0,0 +1,99 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	// SQL Driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+//
+// Query values to be modified by function.
+//
+
+// lookQuery looks up a VALUE from table !A where GUILD_ID=?.
+const lookQuery = `SELECT VALUE FROM !A WHERE GUILD_ID=?;`
+
+// createQuery creates table !A with value VALUE as a TEXT.
+const createQuery = `CREATE TABLE IF NOT EXISTS !A (
+	GUILD_ID INTEGER NOT NULL PRIMARY KEY,
+	VALUE TEXT,
+	);`
+
+// entryQuery inserts a row with values GUILD_ID and VALUE into table !A.
+const insertQuery = `INSERT INTO !A (GUILD_ID, VALUE) VALUES (?,?);`
+
+// updateQuery
+const updateQuery = `UPDATE !A SET VALUE=!B WHERE GUILD_ID=?`
+
+// InitDB initializes the sqlite db.
+func InitDB() error {
+	var err error
+	db, err = sql.Open("sqlite3", "./jb.db")
+	return err
+}
+
+// CreateTable creates tableName Table if it does not exist.
+func CreateTable(tableName string) error {
+	query := strings.Replace(createQuery, "!A", tableName, 1)
+	statement, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	_, err = statement.Exec()
+	return err
+}
+
+// AddEntry adds a Row with Values guildID and entry to Table tableName.
+func AddEntry(guildID int, tableName, entry string) error {
+	lookup, err := LookupEntry(guildID, tableName)
+	if err != nil {
+		query := strings.Replace(insertQuery, "!A", tableName, 1)
+		statement, err := db.Prepare(query)
+		if err != nil {
+			return err
+		}
+		defer statement.Close()
+		statement.Exec(guildID, entry)
+		_, err = statement.Exec()
+		return err
+	} else if lookup != entry {
+		err := updateEntry(guildID, tableName, entry)
+		return err
+	}
+	return nil
+}
+
+// LookupEntry returns the value of a row with
+// a matching guildID from Table tablename.
+func LookupEntry(guildID int, tableName string) (string, error) {
+	var out string
+	query := strings.Replace(lookQuery, "!A", tableName, 1)
+	statement, err := db.Prepare(query)
+	if err != nil {
+		return "", err
+	}
+	defer statement.Close()
+	result, err := statement.Query(guildID)
+	if err != nil {
+		return "", err
+	}
+	defer result.Close()
+	result.Scan(&out)
+	if result.Next() {
+		cols, _ := result.Columns()
+		// Warning because this is not supposed to happen, and it'd need to be manually fixed.
+		fmt.Printf("H3Y D1PSH1T TH3 R3SULT R3TURN3D MOR3 TH4N ON3 T4BL3.\n %v\n F1X TH1S: %v\n", cols, result.Err())
+	}
+	return out, result.Err()
+}
+
+// UpdateEntry updates a row entry.
+func updateEntry(guildID int, tableName, entry string) error {
+
+}
