25bc54090b23e4dace23c439362fef20c9c6e664
octopus octopus <oct2pus@jade.moe>
2020-01-16 01:21:24 -0600

    add emojis.go

---
+++ art/emojis.go
@@ -0,0 +1,11 @@
+package art
+
+// Emoji are shorthand for discord emojicodes.
+var Emoji = map[string]string{
+	"thinking":   "<:jbthink:601863277546569779>",
+	"headpat":    "<:jbheadpat:601863276581748746>",
+	"embarassed": "<:jbembarassed:601863277122813953>",
+	"teefs":      "<:jbteefs:601863276833406976>",
+	"owo":        "<:jbowo:601863276560777220>",
+	"heart":      "💚",
+}
