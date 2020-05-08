62069c74be2a6410cb6029ea11d888cf8df40077
octopus octopus <oct2pus@jade.moe>
2020-05-07 20:56:16 -0500

    fix respond, add response commands, add Mentioned, describe Confused

--- command/respond.go
+++ command/respond.go
@@ -7,14 +7,21 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/oct2pus/bocto"
+	"github.com/oct2pus/jadebot/art"
 	"github.com/oct2pus/jadebot/db"
 )
 
+// Confused is the respond JadeBot provides when the user inputs an incorrect command.
 func Confused(bot bocto.Bot,
 	message *discordgo.MessageCreate,
 	input []string) {
 	respond(bot, message,
 		"i don't understand, maybe you should ask for `"+bot.Prefix+"help` ;P")
+}
+
+// Mentioned is the command that activates when someone @'s JadeBot.
+func Mentioned(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, "hello!\n by the way, my prefix is `"+bot.Prefix+"`! just incase you wanted to know.", art.Emoji["owo"])
 }
 
 // Reminder reminds people to not add spaces between the prefix and command.
@@ -25,8 +32,43 @@ func Reminder(bot bocto.Bot,
 		output := fmt.Sprintf("please don't add a space between commands! try `%v%v` :)", bot.Prefix, strings.Join(input, " "))
 		bot.Session.ChannelMessageSend(message.ChannelID, output)
 	} else {
-		bot.Session.ChannelMessageSend(message.ChannelID, bot.Confused)
+		bot.Confused(bot, message, input)
 	}
+}
+
+// OwO whats this?
+func OwO(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, "oh woah whats this?", art.Emoji["owo"])
+}
+
+// Love is a human emotion.
+func Love(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, "i love you too!!", art.Emoji["teefs"], art.Emoji["heart"])
+}
+
+// GoodDog best friend.
+func GoodDog(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, "best friend", art.Emoji["headpat"])
+}
+
+// Teef teefs.
+func Teef(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, art.Emoji["teefs"])
+}
+
+// KissJade fulfills an **IMPORTANT** business rule for JadeBot.
+func KissJade(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, art.Emoji["embarassed"]+art.Emoji["heart"])
+}
+
+// Headpat feels good. Please give headpats.
+func Headpat(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, art.Emoji["headpat"])
+}
+
+// Thinking 🤔.
+func Thinking(bot bocto.Bot, message *discordgo.MessageCreate, input []string) {
+	respond(bot, message, art.Emoji["thinking"])
 }
 
 // ReminderMarkov reminds people to not add spaces between the prefix and command.
@@ -41,7 +83,7 @@ func ReminderMarkov(bot bocto.Bot,
 	}
 }
 
-func respond(bot bocto.Bot, message *discordgo.MessageCreate, response string) {
+func respond(bot bocto.Bot, message *discordgo.MessageCreate, responses ...string) {
 	i, err := strconv.Atoi(message.GuildID)
 	if err != nil {
 		fmt.Printf("cannot convert guildID number, something really bad happened.\n")
@@ -52,6 +94,8 @@ func respond(bot bocto.Bot, message *discordgo.MessageCreate, response string) {
 		return
 	}
 	if o == "true" || o == "" {
-		bot.Session.ChannelMessageSend(message.ChannelID, response)
+		for _, response := range responses {
+			bot.Session.ChannelMessageSend(message.ChannelID, response)
+		}
 	}
 }
