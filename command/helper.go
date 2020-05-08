b06a0b7c64485e8db8c5fd461be7bd42b1f829e4
octopus octopus <oct2pus@jade.moe>
2020-05-07 21:54:44 -0500

    move internal helper commands to helper.go

---
+++ command/helper.go
@@ -0,0 +1,25 @@
+package command
+
+// ang counts the int32 value of all runes of string 's'
+// and then mods it by 'm'. 'ang' stands for Arbitrary Number Generator.
+func ang(s string, m int32) int32 {
+	runes := []rune(s)
+	var res int32
+
+	for _, ele := range runes {
+		res += ele
+	}
+
+	return res % m
+}
+
+// getJSON returns the response body from a json request.
+func getJSON(uri string) ([]byte, error) {
+	response, err := http.Get(uri)
+	if err != nil {
+		return []byte{}, err
+	}
+	data, err := ioutil.ReadAll(response.Body)
+
+	return data, err
+}
